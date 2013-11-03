e1646a3cca80038af424715420e01e6fcbcf35d4
Daniela Baron <baron.daniela@gmail.com>
2013-11-03 15:21:54 -0500

    angular scaffolding from yo angular plus express view changes

---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanRecipieApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
